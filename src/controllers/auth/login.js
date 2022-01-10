4c5e0b14c6b8588e5af8b319ca7a4a60c0dd64ff
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 15:45:37 +0800

    fix: login lint

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -4,24 +4,20 @@ import { compare } from '../../utils/hash';
 import { tokenConstruct } from '../../utils/token';
 
 export default async (user) => {
-  let mongoUser;
-  let match;
-  let token;
-
   await validateUser(user);
 
-  [mongoUser] = (await userCollection().find({ userName: user.userName }).toArray());
+  const [mongoUser] = (await userCollection().find({ userName: user.userName }).toArray());
   if (!mongoUser) {
     throw new Error('User does not exist');
   }
 
-  match = await compare(user.password, mongoUser.password);
+  const match = await compare(user.password, mongoUser.password);
   if (!match) {
     throw new Error('Wrong password');
   }
 
-  token = await tokenConstruct({
-    id: mongoUser._id // eslint-disable-line no-underscore-dangle
+  const token = await tokenConstruct({
+    id: mongoUser._id, // eslint-disable-line no-underscore-dangle
   });
 
   return { token };
