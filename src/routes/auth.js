4799cf0c3e8a0c299e2b0e78c0c77df31d375e41
Antoine Alejandro <antoine.alejandro@gmail.com>
2022-01-10 15:57:51 +0800

    feat: add verify

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { routeTemplate } from '../utils/routes';
 import register from '../controllers/auth/register';
 import login from '../controllers/auth/login';
+import auth from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/register', routeTemplate(register));
 router.post('/login', routeTemplate(login));
+router.get('/verify', auth, (req, res) => res.json({ message: 'success' }));
 
 export default router;
